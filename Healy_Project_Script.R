deb9839dd14fe689e0ae4f598b138868f614ee1f
unknown <thealy@PC02LQ6R.se.memc.com>
2016-05-02 20:46:22 -0400

    adding project script

---
+++ Healy_Project_Script.R
@@ -0,0 +1,28 @@
+setwd("/Users/TimHealy/Google Drive/MATH 240/Week 8")
+setwd("C:/Users/thealy/Documents/MATH 240/Week 8")
+
+library(plyr)
+library(reshape2)
+library(Quandl)
+
+crime_dat = read.csv("BPD_Part_1_Victim_Based_Crime_Data.csv")
+crime_dat[is.na(crime_dat)] = 0
+crime_dat = ddply(crime_dat, c("Neighborhood", "Description"), summarise, Crimes = sum(Total.Incidents))
+crime_dat = dcast(crime_dat, Neighborhood ~ Description, value.var="Crimes")
+crime_dat[is.na(crime_dat)] = 0
+
+hvi_dat = read.csv("Neighborhood_medianValuePerSqft_AllHomes.csv")
+hvi_dat = subset(hvi_dat, hvi_dat$City == "Baltimore")
+hvi_dat = hvi_dat[,c("RegionName","X2016.03")]
+colnames(hvi_dat)[1] = "Neighborhood"
+
+data = merge(crime_dat, hvi_dat, by = "Neighborhood")
+y = data[,length(colnames(data))]
+m = length(y)
+X = as.matrix(data[,3:length(colnames(data))-1])
+
+theta = as.vector( solve(t(X) %*% X) %*% t(X)%*%y )
+
+result = cbind("Crime" = colnames(X), theta)
+result
+
